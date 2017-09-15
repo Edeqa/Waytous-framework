edd5ee74c60724e56da859d8e8bcf0b083201352
eduardm <eduardm@pantheon-inc.com>
2017-09-15 17:39:32 -0400

    1.41

--- src/main/java/com/edeqa/waytous/Rest.java
+++ src/main/java/com/edeqa/waytous/Rest.java
@@ -9,14 +9,14 @@ package com.edeqa.waytous;
 @SuppressWarnings({"HardCodedStringLiteral", "WeakerAccess"})
 public class Rest {
     public static final String STATUS = "status",
-            SUCCESS = "success",
-            ERROR = "error",
-            REASON = "reason",
-            MESSAGE = "message",
-            REQUEST = "request",
-            GROUP_ID = "group_id",
-            VALUE = "value",
-            PROPERTY = "property",
-            OLD_VALUE = "old_value",
-            USER_NUMBER = "user_number";
+        SUCCESS = "success",
+        ERROR = "error",
+        REASON = "reason",
+        MESSAGE = "message",
+        REQUEST = "request",
+        GROUP_ID = "group_id",
+        VALUE = "value",
+        PROPERTY = "property",
+        OLD_VALUE = "old_value",
+        USER_NUMBER = "user_number";
 }
